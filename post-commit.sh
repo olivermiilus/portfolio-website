8e178917ec0b29c87935bff63d3a1c78e31b31c3
olivermiilus <100768644+olivermiilus@users.noreply.github.com>
2025-01-20 22:10:36 +0000

    delete files

--- post-commit.sh
+++
@@ -1,9 +0,0 @@
-nano post-commit
-#!/bin/sh
-echo "Running post-commit hook..."
-
-# Push committed changes to the remote repository
-git push origin main
-
-# Print success message
-echo "Changes pushed to remote repository."
